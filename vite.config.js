a522576fcf7df7b75b8eb85f46f9b8c67ca914ca
Ross Conover <conover.ross@gmail.com>
2026-08-09 11:47:15 -0400

    add npm stuff

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'MyCardComponents',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      // Ensure external dependencies aren't bundled into your library
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
